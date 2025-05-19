c838e65c1516e1cfb64f7164d272f3ca857d571f
MohammedZaid10 <zaid.diaz6133@gmail.com>
2025-05-20 03:40:38 +0530

    commit

---
+++ Lesson 55 - JavaScript Variables, Data Types & Objects/script.js
@@ -0,0 +1,46 @@
+console.log("This is lesson 55")
+
+var a = 5;
+var b = 6;
+var c = "Harry";
+var _a = 898;
+// var 55a = 28378; not valid variable
+
+console.log(a+b+8)
+console.log(typeof a, typeof b, typeof c)
+
+const a1 = 6;
+// a1 = a1 + 1; Not allowed
+{
+    let a = 55;
+    console.log(a)
+}
+
+console.log(a)
+
+
+// primitive data types
+
+let x = 93;
+let y = "Zaid";
+let z = 3.55;
+let p = true;
+let q = undefined;
+let r = null;
+
+console.log(x,y,z,p,q,r)
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r)
+
+//objects
+
+let o = {
+    "name" : "Zaid",
+    "job code" : 5609,
+    "is_handsome" : true
+}
+
+console.log(o);
+o.salary = '100crores';
+console.log(o);
+o.salary = '500crores';
+console.log(o)
---
+++ Lesson 56 - JavaScript Conditionals - if, else if, else ladder/index.js
@@ -0,0 +1,4 @@
+console.log("Hello i am conditional tutorial")
+
+let age = 5;
+
