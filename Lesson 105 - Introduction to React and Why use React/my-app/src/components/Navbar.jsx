c8db865c445e65d4ef69d29ef38ec2042c4ab7be
MohammedZaid10 <mohammedzaid1012@gmail.com>
2025-07-07 03:50:52 +0530

    commit

--- Lesson 105 - Introduction to React and Why use React/my-app/src/components/Navbar.jsx
+++ Lesson 105 - Introduction to React and Why use React/my-app/src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Footer from './Footer'
 
-const Navbar = () => {
+const Navbar = (props) => {
   return (
     <div>
+      <div className="logo">{props.logoText}</div>
         <ol>
             <li>Home</li>
             <li>About</li>
