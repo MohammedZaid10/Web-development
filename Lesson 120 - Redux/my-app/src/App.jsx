c9c06037b3b434ef4085a0b1fd86b4469ed4964e
MohammedZaid10 <mohammedzaid1012@gmail.com>
2025-07-19 21:07:34 +0530

    Lesson 120 - Redux

--- Lesson 120 - Redux/my-app/src/App.jsx
+++ Lesson 120 - Redux/my-app/src/App.jsx
@@ -2,32 +2,25 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbra from './components/Navbra.jsx'
+import { useSelector, useDispatch } from 'react-redux'
+// added
+import { decrement, increment, multiply } from './redux/counter/counterSlice.js'
+// added
 
 function App() {
-  const [count, setCount] = useState(0)
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
 
   return (
     <>
+    <Navbra />
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button onClick={() => dispatch(decrement())}>-</button>
+        Currently count is {count}
+        <button onClick={() => dispatch(increment())}>+</button>
+        <button onClick={() => dispatch(multiply())}>*</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
